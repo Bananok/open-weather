ae5d7d3f6e2cfabc177acfb31e2aa8b704f24d4d
Ponamarev Anton <antonomskdota@gmail.com>
2022-05-27 01:28:18 +0600

    feat: api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "api.openweathermap.org";
+export const BASE_URL = "http://api.openweathermap.org/";
+export const API_KEY = "881c7d3110082855f058fdd78dce059d";
 
 export const api = axios.create({
   baseURL: BASE_URL,
 });
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error.response.data);
+  }
+);
